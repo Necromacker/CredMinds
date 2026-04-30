36ebafe808c1a91b4839221887c3e3145374649f
Krishna Gorde <krishnagorde04@gmail.com>
2026-04-30 18:08:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,4 +161,5 @@ async def chat(request: Request):
     return result
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8503)
+    port = int(os.environ.get("PORT", 8503))
+    uvicorn.run(app, host="0.0.0.0", port=port)
